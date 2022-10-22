c82a767908be982edf264b7c4dd766e848500b85
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-10-22 18:11:09 -0400

    commit tree b948075f15f7d544a0b903fe5adada1a47aeaa5d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+      if user
+        user.items << Item.create(item_params)
+        render json: user.items.last, status: :created
+      else
+      render json: {error: "item does not belowng to a user"}, status: :not_found
+    end
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_record_not_found
+    render json: {error: "item not found"}, status: :not_found
   end
 
 end
